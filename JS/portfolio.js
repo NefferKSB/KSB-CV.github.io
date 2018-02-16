8010f2c2530a6678c5f03c96715ad9ac78df3229
kennanstanley-bey <kstanleybey@yahoo.com>
2018-02-16 12:12:40 +0100

    Repositioned the bootstrap js script below the jquery js scripts Adjusted contact section styling

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,42 +1,36 @@
-// Site Name:    Portfolio
-// File:         JS
-// Revision:     0.0
-// Author:       Kennan Stanley-Bey
-// Date:         09-02-18
-// Last Update:  11-02-18
 
-// Smooth scrolling
-  function scrollToSection(event) {
-  event.preventDefault();
+ // Smooth scrolling
+ function scrollToSection(event) {
+   event.preventDefault();
 
-  var $section = $($(this).attr("href"));
+   var $section = $($(this).attr("href"));
 
-  $("html, body").animate(
-    {
-      scrollTop: $section.offset().top
-    },
-    1500
-  );
-}
+   $("html, body").animate(
+     {
+       scrollTop: $section.offset().top
+     },
+     1500
+   );
+ }
 
-  $(".scroll-to-section").on("click", scrollToSection);
+ $(".scroll-to-section").on("click", scrollToSection);
 
-// Highlight the top nav as scrolling occurs
+ // Highlight the top nav as scrolling occurs
+$("html, body").scrollspy({ target: ".navbar-fixed-top" });
 
-  $("#topNav").scrollspy(
-    {
-      target: ".navbar-fixed-top"
-    }
-  );
+$('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
 
-$(function() {
-  // Close bootstrap's dropdown menu after clicking
-  $("div.collapse ul.nav li a").each(function() {
-    $(this).on("click", function() {
-      var $obj = $($(this).parents(".in")[0]);
-      $obj.animate({ height: "1px" }, function() {
-        $obj.removeClass("in").addClass("collapse");
-      });
-    });
-  });
-});
+ $(function() {
+   // Close bootstrap's dropdown menu after clicking
+   $("div.collapse ul.nav li a").each(function() {
+     $(this).on("click", function() {
+       var $obj = $($(this).parents(".in")[0]);
+       $obj.animate({ height: "1px" }, function() {
+         $obj.removeClass("in").addClass("collapse");
+       });
+     });
+   });
+ });
